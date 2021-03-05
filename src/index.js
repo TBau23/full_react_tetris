6e3409193714d6cf301a9364216eef77961331fb
Tom Bauer <tbauer1898@gmail.com>
2021-03-05 17:36:34 -0600

    set up store and provider

--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from "./reducers/rootReducer"
+import logger from 'redux-logger';
+
+
+const store = createStore(rootReducer, applyMiddleware(logger))
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider >
+ ,
   document.getElementById('root')
 );
 
