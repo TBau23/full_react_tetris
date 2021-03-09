5d8b11e7617853efcc42ab823e699b1804de8f9e
Tom Bauer <tbauer1898@gmail.com>
2021-03-08 18:37:49 -0600

    disabled controls when game is paused or over

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -5,14 +5,32 @@ import {  move_down, move_left, move_right, rotate } from '../actions/actions';
 const Controls = (props) => {
 
     const dispatch = useDispatch();
-    const isRunning = useSelector((state) => state.game.isRunning);
+    const game = useSelector((state) => state.game);
+    const { isRunning, gameOver} = game;
 
     return(
         <div className="controls">
-            <button className='control-button' onClick={() => { dispatch(move_left())} }>Left</button>
-            <button className='control-button' onClick={() => { dispatch(move_right())}}>Right</button>
-            <button className='control-button' onClick={() => { dispatch(rotate())}}>Rotate</button>
-            <button className='control-button' onClick={() => { dispatch(move_down())}}>Down</button>
+            <button 
+            disabled={!isRunning || gameOver}
+            className='control-button' onClick={() => { 
+                if(!isRunning || gameOver) return
+                dispatch(move_left())}
+                 }>Left</button>
+            <button 
+            disabled={!isRunning || gameOver}
+            className='control-button' onClick={() => { 
+                if(!isRunning || gameOver) return
+                dispatch(move_right())}}>Right</button>
+            <button 
+            disabled={!isRunning || gameOver}
+            className='control-button' onClick={() => { 
+                if(!isRunning || gameOver) return
+                dispatch(rotate())}}>Rotate</button>
+            <button 
+            disabled={!isRunning || gameOver}
+            className='control-button' onClick={() => { 
+                if(!isRunning || gameOver) return
+                dispatch(move_down())}}>Down</button>
         </div>
     )
 }
